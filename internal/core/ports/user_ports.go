4c33dceb5cf11e03a466d9a3c38d3cf41de751e5
patipan <patipan.ar65@snru.ac.th>
2025-06-10 11:08:47 +0700

    pert: edit user_post

--- internal/core/ports/user_ports.go
+++ internal/core/ports/user_ports.go
@@ -6,31 +6,25 @@ import "github.com/PatipanDev/mini-project-golang/internal/core/domain"
 type UserRepository interface {
 	Create(user *domain.User) error
 	FindByEmail(email string) (*domain.User, error)
-<<<<<<< HEAD
 	Update(user *domain.User, id string) error
 	Delete(id string) error
 	FindUserById(id string) (*domain.User, error)
-=======
 	SearchData(query string) ([]domain.User, error)
 	Get() ([]domain.User, error)
 	FindAll(offset int, limit int) ([]domain.User, error)
 	Count() (int64, error)
 	//filter
 	FindUsers(filter *domain.UserFilter) ([]domain.User, int64, error)
->>>>>>> origin/dev-nueng
 }
 
 //Primary ports
 type UserService interface {
 	RegisterUser(user *domain.User) error
-<<<<<<< HEAD
 	UpdateUser(user *domain.User, id string) error
 	DeleteUser(id string) error
 	GetUserById(id string) (*domain.User, error)
-=======
 	SearchUser(query string) ([]domain.User, error)
 	FindUsers() ([]domain.User, error)
 	GetPaginationUsers(page int, limit int) (*domain.Pagination, error)
 	GetUsers(filter *domain.UserFilter) ([]domain.User, int64, error)
->>>>>>> origin/dev-nueng
 }
